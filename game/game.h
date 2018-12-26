e009fa4e5a6c3640e3eb056ddc28ce1dccfad483
xdu <duxuefengv@foxmail.com>
2018-12-26 22:32:57 +0800

    俄罗斯方块

---
+++ game/game.h
@@ -0,0 +1,32 @@
+﻿
+// game.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CgameApp:
+// 有关此类的实现，请参阅 game.cpp
+//
+
+class CgameApp : public CWinApp
+{
+public:
+	CgameApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CgameApp theApp;
---
+++ game/gameDlg.h
@@ -0,0 +1,33 @@
+﻿
+// gameDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CgameDlg 对话框
+class CgameDlg : public CDialogEx
+{
+// 构造
+public:
+	CgameDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GAME_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
